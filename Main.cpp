a99a1e00063887e170df8dad7f01b13fe8591bbf
patel-esha <139727766+patel-esha@users.noreply.github.com>
2023-07-31 14:37:54 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -47,3 +47,48 @@ Image Multiply(Image &A, Image &B)
 
 }
 
+
+// Function to apply subtraction blending tool
+Image Subtract(Image &A, Image&B)
+{
+    Image C;
+    Image::Header newHeader = A.AccessHeader();
+    C.AdjustHeader(newHeader);
+
+    vector<Pixel> VectorA = A.AccessVector();
+    vector<Pixel> VectorB = B.AccessVector();
+
+    for (unsigned int i = 0; i < VectorA.size(); i++)
+    {
+        VectorA[i].blueInt = (int)((VectorA[i].blue) - '\0');
+        VectorA[i].greenInt = (int)((VectorA[i].green) - '\0');
+        VectorA[i].redInt = (int)((VectorA[i].red) - '\0');
+    }
+
+    for (unsigned int i = 0; i < VectorB.size(); i++)
+    {
+        VectorB[i].blueInt = (int)((VectorB[i].blue) - '\0');
+        VectorB[i].greenInt = (int)((VectorB[i].green) - '\0');
+        VectorB[i].redInt = (int)((VectorB[i].red) - '\0');
+    }
+
+    vector<Pixel> VectorC;
+
+    for (unsigned int i = 0; i < VectorA.size(); i++)
+    {
+        Pixel PixelC;
+
+        PixelC.blue = ClampConvert(VectorA[i].blueInt - VectorB[i].blueInt);
+        PixelC.green = ClampConvert(VectorA[i].greenInt - VectorB[i].greenInt);
+        PixelC.red = ClampConvert(VectorA[i].redInt - VectorB[i].redInt);
+
+        VectorC.push_back(PixelC);
+
+    }
+    C.AdjustVector(VectorC);
+    return C;
+}
+
+
+
+
